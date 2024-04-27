b8053253089d1442d573145f65c3f9a499409e45
Santosh Kumar Pati <patisantosh00@gmail.com>
2024-04-28 00:54:38 +0530

    added join event in backend

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,24 +1,41 @@
 import express from "express";
-import { createServer } from "node:http";
-import { Server } from "socket.io";
+import { createServer } from "http";
+import { Server, Socket } from "socket.io";
+import { ACTIONS } from "./Actions";
 
 const app = express();
 const server = createServer(app);
 const io = new Server(server);
 
-app.get("/", (req, res) => {
-  res.status(200).json({
-    message: "Hello from server",
-  });
-});
+
+
+type usermap = {
+  [socketId: string]: string,
+}
+const userSocketMap: usermap = {};
+
+function getRoomClients(roomid:string){
+  return Array.from(io.sockets.adapter.rooms.get(roomid) || []).map((socketId)=>{
+    return {socketId,username:userSocketMap[socketId]}
+  })
+}
 
 io.on("connection", (socket) => {
-  console.log("a user connected",socket.id);
 
-  socket.on("message", (msg) => {
-    io.emit("message" , msg);
-    console.log(msg)
+  socket.on(ACTIONS.JOIN, ({ username, roomID }: { username: string, roomID: string }) => {
+    console.log(username)
+    userSocketMap[socket.id] = username;
+    socket.join(roomID);
+    const clients = getRoomClients(roomID);
+    clients.forEach(({socketId})=>{
+      io.to(socketId).emit(ACTIONS.JOINED,{
+        clients,  
+        username,
+        socketId:socket.id
+      })
+    })
   });
+   
 });
 
 const port = process.env.PORT || 5000;
--- frontend/src/pages/Editor.tsx
+++ frontend/src/pages/Editor.tsx
@@ -1,4 +1,4 @@
-import { useSearchParams } from "react-router-dom";
+import { Navigate, useNavigate, useSearchParams } from "react-router-dom";
 import { Clients } from "../components/Clients";
 import logo from '../assets/code-cast-high-resolution-logo-transparent.png';
 import { useEffect, useRef, useState } from "react";
@@ -15,25 +15,37 @@ type Client = {
 const Editor = () => {
   const [sidebarVisible, setSidebarVisible] = useState(false);
   const [searchParams] = useSearchParams();
-  const username = searchParams.get('username');
+  const userName = searchParams.get('username');
   const roomID = searchParams.get('roomID');
   const socketRef = useRef<null | Socket>(null);
-  const [clients, setClients] = useState<Client[]>([
-    { SocketId: 1, username: "Raman" },
-    { SocketId: 2, username: "Dinesh" },
-    { SocketId: 3, username: "Santosh" },
-  ]);
+  const reactnavigate = useNavigate();
+  const [clients, setClients] = useState<Client[]>([]);
 
   useEffect(()=>{
     const init = async()=>{
       socketRef.current = await initSocket();
+      socketRef.current.on('connect_error',(e)=> handleError(e));
+      socketRef.current.on('connect_failed',(e)=> handleError(e));
+
       socketRef.current.emit(ACTIONS.JOIN,{
         roomID,
-        username,
+        username:userName,
       })
+
+      
     }
     init();
   },[])
+
+  function handleError(e:Event){
+    console.log('socket error',e)
+    alert('Socket connnection failed!');
+    reactnavigate('/auth')
+  }
+
+  if(!userName || !roomID){
+    <Navigate to={'/auth'}/>
+  }
 
 
   return (
