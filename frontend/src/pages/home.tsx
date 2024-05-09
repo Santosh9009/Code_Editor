d742bc4aa858eddd3dda2d3d51969abe86a23ee8
Santosh Kumar Pati <patisantosh00@gmail.com>
2024-05-10 00:50:25 +0530

    aded some gradient on hero page

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,18 +1,22 @@
+import { Link } from 'react-router-dom';
 import logo from '../assets/code-cast-high-resolution-logo-transparent.png'
+import './home.css'
 const Home = () => {
+
+
   return (
     <div>
       <nav className="bg-black p-6 w-full">
         <div className="flex justify-between items-center">
           <img className='h-4 md:h-7' src={logo} alt="" />
           {/* Add more navigation links here if needed */}
-          <button className='bg-slate-300 px-4 py-2 rounded-full font-semibold hover:scale-105 duration-200 hover:bg-white'>Get Started</button>
+          <button className='bg-slate-300 px-4 py-2 rounded-full font-semibold hover:scale-105 duration-200 hover:bg-white'><Link to={'/auth'}>Get Started</Link></button>
         </div>
       </nav>
-      <div className="min-h-screen w-full flex flex-col justify-center items-center bg-gradient-to-b from-black to-blue-900 text-white gap-10 text-center">
-        <h1 className="text-6xl font-bold px-5">Code Together, Anytime, Anywhere!</h1>
-        <p className="text-lg text-center max-w-5xl mx-auto">Welcome to <span className='text-[#45e7f9]'>CODECAST</span>, where coding becomes a collaborative adventure! Whether you're a solo developer seeking feedback or a team striving for seamless collaboration, we've got you covered.</p>
-        <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-3 px-6 rounded-full transition duration-300 ease-in-out transform hover:scale-105">Start Coding Together Today!</button>
+      <div className="min-h-screen w-full flex flex-col justify-center items-center text-white gap-10 text-center home">
+        <h1 className="text-2xl md:text-6xl font-semibold px-5 font-roboto gradient-text">Code Together, Anytime, Anywhere!</h1>
+        <p className="text-base md:text-lg text-center mx-5 md:max-w-2xl font-sans">Welcome to <span className='text-[#45e7f9] font-mono'>CODECAST</span>, your collaborative coding hub! Conduct interviews, solve doubts, and tackle problems together effortlessly.</p>
+        <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-3 px-6 rounded-full transition duration-300 ease-in-out transform hover:scale-105"><Link to={'/auth'}>Start Coding Together Today!</Link></button>
       </div>
     </div>
   );
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        'poppins':['Poppins','sans-serif'],
+        'montserrat':['Montserrat','sans-serif'],
+        'roboto':['Roboto','sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
